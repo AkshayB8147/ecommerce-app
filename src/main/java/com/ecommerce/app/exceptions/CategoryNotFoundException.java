8f58ff5aab56a96079cdbaaf15f6d41311e72e87
AkshayB8147 <tbhandge6@gmail.com>
2023-06-22 16:02:16 +0530

    catalog bugs fixed

--- src/main/java/com/ecommerce/app/exceptions/CategoryNotFoundException.java
+++ src/main/java/com/ecommerce/app/exceptions/CategoryNotFoundException.java
@@ -1,25 +1,7 @@
 package com.ecommerce.app.exceptions;
 
 public class CategoryNotFoundException extends RuntimeException {
-<<<<<<< HEAD
     public CategoryNotFoundException(String message) {
         super(message);
     }
-=======
-    public CategoryNotFoundException() {
-        super();
-    }
-
-    public CategoryNotFoundException(String message) {
-        super(message);
-    }
-
-    public CategoryNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public CategoryNotFoundException(Throwable cause) {
-        super(cause);
-    }
->>>>>>> origin/main
 }
